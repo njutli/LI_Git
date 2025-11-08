c94664ad30423de4a9b363fc3460da8c833f5b48
njutli <1151488180@qq.com>
2025-11-08 11:00:22 +0800

    Update bpf_simple.c

--- 其他/bpf/bpf_simple.c
+++ 其他/bpf/bpf_simple.c
@@ -1,6 +1,11 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+/*
+SEC() 宏的作用：
+告诉编译器：把这个函数放到一个特殊的 ELF section 中，section 名字是 tracepoint/syscalls/sys_enter_execve。
+可通过 readelf -S bpf_simple.o 验证
+*/
 SEC("tracepoint/syscalls/sys_enter_execve")
 int trace_execve(void *ctx)
 {
